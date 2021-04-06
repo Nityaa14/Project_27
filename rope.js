60a634af79e585f410e9684558a8aef60654a2b8
Nityaa14 <73293921+Nityaa14@users.noreply.github.com>
2021-04-06 10:43:27 +0530

    Add files via upload

---
+++ rope.js
@@ -0,0 +1,30 @@
+class Rope{
+    constructor(bodyA, bodyB, offsetX, offsetY){
+        
+        this.offsetX = offsetX;
+        this.offsetY = offsetY;
+        var options = {
+            bodyA: bodyA,
+            bodyB : bodyB,
+            pointB : {x: this.offsetX, y: this.offsetY},
+            stiffness: 0.04,
+            length: 80
+        }
+        //this.pointB = pointB;
+        this.rope = Constraint.create(options);
+        World.add(world, this.rope);
+    }
+
+    display(){
+        if(this.rope.bodyA){
+            var pointA = this.rope.bodyA.position;
+        var pointB = this.rope.bodyB.position;
+        strokeWeight(2);
+        line(pointA.x, pointA.y, pointB.x + this.offsetX, pointB.y + this.offsetY);
+        }
+    }
+     Fly() {
+         this.rope.bodyA = null;
+     }
+    
+}
